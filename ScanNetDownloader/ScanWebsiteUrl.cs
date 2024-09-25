ace228d704a82fe423dccf1a6f0e0e2cb801dea6
RomainSepulchre <romain.sepulchre@outlook.com>
2024-09-24 21:53:08 -0400

    WIP: Start a refactoring to have a better handling of multiple scan website

---
+++ ScanNetDownloader/ScanWebsiteUrl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanNetDownloader
+{
+    public abstract class ScanWebsiteUrl
+    {
+        public string url;
+
+        public string WebsiteName
+        {
+             get; protected set;
+        }
+
+        public ScanWebsiteUrl(string url)
+        {
+            this.url = url;
+        }
+
+        public abstract string ParseHtmlToGetImgLinks(string httmlContent, string htmlUrl);
+
+        public abstract string GetBookNameFromUrl(string url, bool removeSpace=false);
+
+        public abstract string GetChapterNumberFromUrl(string url, bool keepNumberOnly=true);
+
+        public abstract string GetFileExtensionFromUrl(string url);
+
+    }
+}
